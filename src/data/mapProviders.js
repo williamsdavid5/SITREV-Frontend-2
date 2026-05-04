61392d91fd4ad286cf18e54f070aa61efeb3cefb
David Duarte (Williams) <davidduart04@gmail.com>
2026-05-04 16:48:28 -0300

    os providers foram extraidos, adicionados registros ficticios no sistema

---
+++ src/data/mapProviders.js
@@ -0,0 +1,68 @@
+const mapaProviders = {
+    openstreetmap_france: {
+        name: "OpenStreetMap France",
+        url: "https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png",
+        attribution: '&copy; OpenStreetMap France | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        maxZoom: 20
+    },
+
+    openstreetmap_hot: {
+        name: "OpenStreetMap HOT",
+        url: "https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>',
+        maxZoom: 19
+    },
+    cyclosm: {
+        name: "CyclOSM",
+        url: "https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png",
+        attribution: '<a href="https://github.com/cyclosm/cyclosm-cartocss-style/releases" title="CyclOSM - Open Bicycle render">CyclOSM</a> | Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        maxZoom: 20
+    },
+    esri: {
+        name: "Esri Satélite",
+        url: "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+        attribution: "Tiles © Esri — Source: Esri, USGS, NOAA",
+        maxZoom: 19
+    },
+    openstreetmap: {
+        name: "Open street",
+        url: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        maxZoom: 19
+    },
+    memomaps: {
+        name: "Memomaps",
+        url: "https://tileserver.memomaps.de/tilegen/{z}/{x}/{y}.png",
+        attribution: 'Map <a href="https://memomaps.de/">memomaps.de</a> <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        maxZoom: 18
+    },
+    opentopomap: {
+        name: "OpenTopoMap",
+        url: "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png",
+        attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
+        maxZoom: 17
+    },
+    stadia: {
+        name: "Satélite",
+        url: "https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.jpg",
+        attribution: '© CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | © <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> © <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> © <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        maxZoom: 20,
+        key: "494875da-da82-4808-9998-d52ad50dd6ba"
+    },
+    openstreetmap_cat: {
+        name: "OpenStreetMap CAT",
+        url: "https://tile.openstreetmap.bzh/ca/{z}/{x}/{y}.png",
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles courtesy of <a href="https://www.openstreetmap.cat" target="_blank">Breton OpenStreetMap Team</a>',
+        maxZoom: 19
+    },
+    esri_world_street: {
+        name: "Esri World Street",
+        url: "https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}",
+        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012',
+        maxZoom: 19
+    },
+
+    default: 'openstreetmap_hot'
+};
+
+export default mapaProviders;
--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,11 +1,57 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, Polyline } from 'react-leaflet';
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-import { useEffect } from 'react';
+
+import { useEffect, useState } from 'react';
+import viajens from '../data/rastreamento';
 
 import './inicio.css'
 import Mapa from './components/Mapa';
 
+import veiculoIcon from '../assets/veiculoIcon.png';
+import pontoIcon from '../assets/pontoIcon.png'
+import startIcon from '../assets/startIcon.png';
+import alertaIcon from '../assets/alertaIcon.png';
+
+const vehicleIcon = new L.Icon({
+    iconUrl: veiculoIcon,
+    iconSize: [40, 40],
+    iconAnchor: [15, 15],
+    className: 'iconeVeiculo'
+});
+
+const pontoPercursoIcon = new L.Icon({
+    iconUrl: pontoIcon,
+    iconSize: [35, 35],
+    iconAnchor: [15, 15],
+    popupAnchor: [0, -15],
+    className: 'pontoIcon'
+});
+
+const starPercursotIcon = new L.Icon({
+    iconUrl: startIcon,
+    iconSize: [35, 35],
+    iconAnchor: [15, 15],
+    popupAnchor: [0, -15],
+    className: 'startIcon'
+});
+
+const alertIcon = new L.Icon({
+    iconUrl: alertaIcon,
+    iconSize: [30, 30],
+    iconAnchor: [15, 15],
+    className: 'iconeAlerta'
+});
+
 export default function Inicio() {
+    const [dados, setDados] = useState(viajens);
+    const [viagemSelecionada, setViagemSelecionada] = useState(null);
+
+    // para mostrar uma viagem por vez no mapa
+    const alternarPercurso = (id) => {
+        setViagemSelecionada(viagemSelecionada === id ? null : id);
+    };
+
     useEffect(() => {
         document.title = "SITREV - Início";
     }, [])
@@ -14,8 +60,99 @@ export default function Inicio() {
     return (
         <>
             <main className="inicioMain">
-                <Mapa center={position} zoom={10}>
-                    {/* <Marker position={position} /> */}
+                <Mapa center={position} zoom={15}>
+
+                    {dados.map((viagem) => {
+                        const registros = viagem.registros;
+                        const ultimoRegistro = registros[registros.length - 1];
+                        const isSelecionada = viagemSelecionada === viagem.id;
+
+                        // Coordenadas para a Polyline
+                        const rotaCoordenadas = registros.map(reg => [
+                            parseFloat(reg.latitude),
+                            parseFloat(reg.longitude)
+                        ]);
+
+                        return (
+                            <div key={viagem.id}>
+                                {/* 1. Renderiza o Percurso APENAS se a viagem estiver selecionada */}
+                                {isSelecionada && (
+                                    <>
+                                        <Polyline
+                                            positions={rotaCoordenadas}
+                                            pathOptions={{
+                                                color: '#3498db',
+                                                dashArray: '10, 10',
+                                                weight: 3
+                                            }}
+                                        />
+
+                                        {/* Pontos intermediários (exceto o último que é o veículo) */}
+                                        {registros.slice(0, -1).map((reg, index) => {
+                                            const isFirst = index === 0;
+                                            return (
+                                                <Marker
+                                                    key={reg.id}
+                                                    position={[parseFloat(reg.latitude), parseFloat(reg.longitude)]}
+                                                    icon={isFirst ? starPercursotIcon : pontoPercursoIcon}
+                                                >
+                                                    <Popup>
+                                                        <strong>{isFirst ? 'Início do Percurso' : 'Ponto de Percurso'}</strong><br />
+                                                        Velocidade: {reg.velocidade} km/h<br />
+                                                        Hora: {new Date(reg.timestamp).toLocaleTimeString()}
+                                                    </Popup>
+                                                </Marker>
+                                            );
+                                        })}
+                                    </>
+                                )}
+
+                                {/* 2. Veículo (Sempre visível - Último ponto) */}
+                                <Marker
+                                    position={[parseFloat(ultimoRegistro.latitude), parseFloat(ultimoRegistro.longitude)]}
+                                    icon={vehicleIcon}
+                                    eventHandlers={{
+                                        click: () => alternarPercurso(viagem.id),
+                                    }}
+                                >
+                                    <Popup>
+                                        <strong>Veículo: {viagem.identificador_veiculo}</strong><br />
+                                        Velocidade: {ultimoRegistro.velocidade} km/h<br />
+                                        Hora: {new Date(ultimoRegistro.timestamp).toLocaleTimeString()}<br />
+                                        Status: {isSelecionada ? "📍 Exibindo Percurso" : "👆 Clique para ver percurso completo"}
+                                    </Popup>
+                                </Marker>
+
+                                {/* 3. Alertas (Sempre visíveis, posicionados no último registro) */}
+                                {viagem.alertas && viagem.alertas.length > 0 && viagem.alertas.map((alerta) => {
+                                    return (
+                                        <Marker
+                                            key={`alerta-${alerta.id}`}
+                                            position={[
+                                                parseFloat(ultimoRegistro.latitude),
+                                                parseFloat(ultimoRegistro.longitude)
+                                            ]}
+                                            icon={alertIcon}
+                                        >
+                                            <Popup>
+                                                <div style={{ minWidth: '150px' }}>
+                                                    <strong style={{ color: 'red', display: 'block', marginBottom: '5px' }}>
+                                                        ⚠️ ALERTA: {alerta.tipo}
+                                                    </strong>
+                                                    <p style={{ margin: '0', fontSize: '12px' }}>
+                                                        {alerta.descricao}
+                                                    </p>
+                                                    <small style={{ color: '#666' }}>
+                                                        Hora: {new Date(alerta.timestamp).toLocaleTimeString()}
+                                                    </small>
+                                                </div>
+                                            </Popup>
+                                        </Marker>
+                                    );
+                                })}
+                            </div>
+                        );
+                    })}
                 </Mapa>
             </main>
         </>
